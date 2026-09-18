23d5316f3febe54cafb8ae055d4ad96072cd760c
skillmux-release <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-18 06:09:32 +0000

    chore(release): update package manifests for v0.2.0

--- Formula/skillmux.rb
+++ Formula/skillmux.rb
@@ -1,25 +1,25 @@
 class Skillmux < Formula
   desc "Local-first SKILL.md manager across agent harnesses"
   homepage "https://github.com/Auro-rium/skillmux"
-  version "0.1.0"
+  version "0.2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Auro-rium/skillmux/releases/download/v0.1.0/skillmux_darwin_arm64.tar.gz"
-      sha256 "c777ac6a43e8822771b01032271ebc03bbf7df2ef886834c6ad354572aee62a9"
+      url "https://github.com/Auro-rium/skillmux/releases/download/v0.2.0/skillmux_darwin_arm64.tar.gz"
+      sha256 "cbbe08c9184fd631efcb96b00ce2adc279f54233fc1c7da01961eb98e8475aab"
     else
-      url "https://github.com/Auro-rium/skillmux/releases/download/v0.1.0/skillmux_darwin_amd64.tar.gz"
-      sha256 "8ca1dff9ebb7898a574ccc0fcfe059489ebcf471a7b33154b67b592aa182ba11"
+      url "https://github.com/Auro-rium/skillmux/releases/download/v0.2.0/skillmux_darwin_amd64.tar.gz"
+      sha256 "502777f6ec9cf0b242514d0b7a17c77db32e707af7b6dc8cf146b3bef63c494b"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Auro-rium/skillmux/releases/download/v0.1.0/skillmux_linux_arm64.tar.gz"
-      sha256 "069b2a898066078fcd5b043a98bba4c430854a6581bd1f1b9056541d4fef0f1b"
+      url "https://github.com/Auro-rium/skillmux/releases/download/v0.2.0/skillmux_linux_arm64.tar.gz"
+      sha256 "41537d50404ddcb7fe848eacdd51cc87aae3cdf27f5decea74a189cf64e767bd"
     else
-      url "https://github.com/Auro-rium/skillmux/releases/download/v0.1.0/skillmux_linux_amd64.tar.gz"
-      sha256 "d210b28ba264252d855192fed2573d18f4cc1b3ee8c37cdf57a775c649612c06"
+      url "https://github.com/Auro-rium/skillmux/releases/download/v0.2.0/skillmux_linux_amd64.tar.gz"
+      sha256 "6cee96439d42f38e147c1e6b74b539c3361b9fabb542a4a75a13c99130a0c6e5"
     end
   end
 
